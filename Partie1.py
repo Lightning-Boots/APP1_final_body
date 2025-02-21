d24aa1c10ead962fa60bf618ca1ca8ec6fb7e986
Sixtine <sixtine.malbrunot@gmail.com>
2025-02-21 15:02:17 +0100

    changes

--- Partie1.py
+++ Partie1.py
@@ -1,7 +1,6 @@
 # définir le plateau de jeu
-n=int(input("nombre d'allumettes"))
-board = ["|" for i in range(n)]
-print(board)
+
+
 
 # L'ordinateur joue aléatoirement
 import random
@@ -19,35 +18,15 @@ def play_random_move_V2(board):
     a = random.randint(1, 2)
   elif len(board)==1:
     return "Tu as perdu"
-  else len(board)==0:
+  else:
     return "Fin, tu as gagné"
   return a, board[a:]
 
 
 
-
-a, newboard = play_random_move(board)
+board = ["|" for i in range(13)]
+print(board)
+a, newboard = play_random_move_V2(board)
 print(a, newboard)
-a, newboard1 = play_random_move(newboard)
+a, newboard1 = play_random_move_V2(newboard)
 print(a, newboard1)
-
-
-#Autre options: classe orientée objet
-class Board:
-
-  def __init__(self):
-    self.board = ["|" for i in range(13)]
-
-  def play_random_move(self):
-    if is_game_over(self)==False:
-        a = random.randint(1, 3)
-        return a, self.board[a:]
-    else:
-      return "Game Over"
-
-  # en plus
-  def play_human_move(self, move):
-    return self.board[move:]
-
-  def is_game_over(self):
-    return len(self.board) == 0
